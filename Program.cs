274f0988173efad369225b500cd37702e0bf5a30
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-02-27 15:35:58 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace helloword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            float f = 20.5f;
+            double d = 20.5;
+            Boolean b = true;
+            char c = 'a';
+            String s = "Hà Nội";
+            Console.WriteLine(i);
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(s);
+
+        }
+    }
+}
